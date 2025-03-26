ee6aee1fd40b238516c1e9c00df80da21f09a764
Kerem Türközü <turkozukerem@gmail.com>
2025-03-26 23:11:08 +0300

    Initial Commit

---
+++ FuelTrackerApp/FuelTrackerAppApp.swift
@@ -0,0 +1,32 @@
+//
+//  FuelTrackerAppApp.swift
+//  FuelTrackerApp
+//
+//  Created by Kerem Türközü on 26.03.2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct FuelTrackerAppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
